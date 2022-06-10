47c19d1de15134dcf625523a532c5dc9444fc11d
dex1e <dex1e5@yandex.ru>
2022-06-11 02:02:01 +0600

    adaptive finished + validation form

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// let form = document.querySelector(".form"),
+//     formInputs = document.querySelectorAll(".req")
+
+// form.onsubmit = function () {
+//     formInputs.forEach(function (input) {
+//         if (input.value === '') {
+//             input.classlist.add('error')
+//             console.log("input not field")
+//         } else {
+//             input.classList.remove('error')
+//         }
+//     })
+
+//     return false
+
+// }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form')
+    form.addEventListener('submit', formSend)
+
+    async function formSend(e) {
+        e.preventDefault()
+
+        let error = formValidate(form)
+        if (error === 0) {
+            for (let i = 0; i < 7; i++) {
+                console.log(e.target[i].value)
+            }
+            form.reset()
+        }
+        else {
+            alert('Необходимо заполнить обязательные поля')
+        }
+    }
+
+    function formValidate(form) {
+        let error = 0
+        let formReq = document.querySelectorAll('.req')
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index]
+            formRemoveError(input)
+
+            if (input.value === '') {
+                formAddError(input)
+                error++
+            }
+        }
+        return error
+    }
+    function formAddError(input) {
+        input.classList.add('_error')
+    }
+    function formRemoveError(input) {
+        input.classList.remove('_error')
+    }
+});
